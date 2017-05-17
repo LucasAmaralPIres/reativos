54f3183b499298994e385e72b6390f18819b1548
LucasAmaralPires <lucasampires@gmail.com>
2017-05-17 10:17:20 -0300

    Conserta Led acaba, interromper buzina;

--- Mini-Arduino/Projeto/Projeto.ino
+++ Mini-Arduino/Projeto/Projeto.ino
@@ -10,6 +10,7 @@ byte mode;
 byte flag_button_blink;
 unsigned long int segundos[4];
 unsigned long int index_display;
+unsigned long int temp_antes;
 int seg;
 byte rel_pisc;
 
@@ -18,16 +19,20 @@ void write_display()
   char disp[] = {'0','0','0','0'};
   if((mode == REL) || (mode ==  AL) || (mode == TIM))
   {
-    disp[0] = segundos[mode]/36000 + 48;
-    int rest1 = segundos[mode]%36000;
-    disp[1] = rest1/3600 + 48;
-    int rest2 = rest1%3600;
-    disp[2] = rest2/600 + 48;
-    int rest3 = rest2%600;
-    disp[3] = rest3/60 + 48;
+    disp[0] = segundos[mode]/36000 + '0';
+    Serial.println(segundos[mode]);
+    unsigned long int rest1 = segundos[mode]%36000;
+    Serial.println(rest1);
+    Serial.println(rest1/3600);
+    disp[1] = rest1/3600 + '0';
+    unsigned long int rest2 = rest1%3600;
+    disp[2] = rest2/600 + '0';
+    unsigned long int rest3 = rest2%600;
+    disp[3] = rest3/60 + '0';
     MFS.write(disp);
   }
 }
+
 void blink_display()
 {
   switch(index_display)
@@ -56,7 +61,21 @@ void button_pressed (byte button)
   {
     case BUTTON_1_SHORT_RELEASE:
         if(!flag_button_blink)
+        {
           mode++;
+          temp_antes = segundos[mode];
+        } 
+        break;
+    case BUTTON_2_PRESSED:
+        if(index_display == 6)
+        {
+          flag_button_blink = !flag_button_blink;
+          rel_pisc = 0;
+          index_display = 36000;
+          temp_antes = segundos[mode];
+          segundos[mode] = 0;
+          MFS.blinkDisplay(DIGIT_ALL,OFF);
+        }
         else
         {
           if (index_display != 3600)
@@ -71,29 +90,16 @@ void button_pressed (byte button)
           }
           else
             index_display = index_display/6;
-        }
-        break;
-    case BUTTON_1_LONG_RELEASE:
-        flag_button_blink = !flag_button_blink;
-        rel_pisc = 0;
-        index_display = 36000;
-        MFS.blinkDisplay(DIGIT_ALL,OFF);
-        break;
-    case BUTTON_2_PRESSED:
-        if(flag_button_blink == 1)
-        {
-          if((mode == REL) || (mode ==  AL) || (mode == TIM))
-          {
-            segundos[mode] += index_display;
-          }
+          temp_antes = segundos[mode];
         }
         break;
     case BUTTON_3_PRESSED:
         if(flag_button_blink == 1)
         {
-          if(((mode == REL) || (mode ==  AL) || (mode == TIM)) && (index_display < segundos[mode]))
+          if((mode == REL) || (mode ==  AL) || (mode == TIM))
           {
-            segundos[mode] -= index_display;
+            segundos[mode] += index_display;
+            check_out_of_bounds(mode);
           }
         }
         break;
@@ -120,7 +126,10 @@ void controle_led()
       MFS.blinkLeds(LED_2,OFF);
   }
   else
+  {
+    MFS.blinkLeds(LED_2,OFF);
     MFS.writeLeds(LED_2,ON);
+  }
   if(mode != TIM)
   {
     if(segundos[TIM]>0)
@@ -130,6 +139,7 @@ void controle_led()
   }
   else
   {
+    MFS.blinkLeds(LED_3,OFF);
     MFS.writeLeds(LED_3,ON);
     if(segundos[AL] == 0)
       MFS.writeLeds(LED_2,OFF);
@@ -143,11 +153,50 @@ void controle_led()
   }
   else
   {
+    MFS.blinkLeds(LED_4,OFF);
     MFS.writeLeds(LED_4,ON);
     if(segundos[TIM] == 0)
       MFS.writeLeds(LED_3,OFF);
   }
 }
+
+void check_out_of_bounds(byte i)
+{
+  
+  if (index_display == 60)
+  {
+    if(((segundos[i]-temp_antes)/60) >=10)
+    {
+      segundos[i] -= 600;
+    }
+  }
+  else if (index_display == 600)
+  {
+    if(((segundos[i]-temp_antes)/600) >=6)
+    {
+      segundos[i] -= 3600;
+    }
+  }
+  else if (index_display == 3600)
+  {
+    if(((segundos[i]-temp_antes)/3600) >=10)
+    {
+      segundos[i] -= 36000;
+    }
+    else if((segundos[i]>=72000) && ((segundos[i]-temp_antes)/3600) >=4)
+    {
+      segundos[i] -= 14400;
+    }
+  }
+  else if (index_display == 36000)
+  {
+    if(segundos[i]>=108000)
+    {
+      segundos[i] -= 108000;
+    }
+  }  
+}
+
 void setup() 
 {
  Timer1.initialize();
@@ -165,13 +214,31 @@ void setup()
 void loop() 
 {
   delay(10);
-  seg++;
-  if(seg == 100)
+  byte ja_tocou;
+  if(index_display == 6)
   {
-    seg = 0;
-    segundos[REL] += 1;
-    if(segundos[TIM] > 0)
-      segundos[TIM] -= 1;
+    seg++;
+    if(seg == 100)
+    {
+      seg = 0;
+      segundos[REL] += 1;
+      if(segundos[TIM] > 0)
+      {
+        segundos[TIM] -= 1;
+        byte ja_tocou = 1;
+      }
+      if(segundos[REL] >= 86400)
+        segundos[REL] = segundos[REL] - 86400;
+      if(segundos[TIM] == 60)
+        MFS.beep(10,10,0,2,300);
+      else if((segundos[TIM] == 0) && (ja_tocou))
+      {
+        ja_tocou = 0;
+        MFS.beep(75,75,10,1,500);
+      }
+      if(segundos[AL] == segundos[REL])
+        MFS.beep(10,10,5,5,100);
+    }
   }
   byte but = MFS.getButton();
   button_pressed(but);
@@ -183,45 +250,15 @@ void loop()
     MFS.blinkDisplay(DIGIT_ALL, ON);
   else if(index_display == 6)
     MFS.blinkDisplay(DIGIT_ALL,OFF);
-  for(int i = 0;i<4;i++)
-  {
-    if((segundos[i] >= 86400) && (index_display == 6))
-      segundos[i] = segundos[i] - 86400;
-  /*  else if (index_display == 60)
-    {
-      if((segundos[i]%600)>540)
-      {
-        segundos[i] -= 540;
-      }
-    }
-    else if (index_display == 600)
-    {
-      if((segundos[i]%3600)>3000)
-      {
-        segundos[i] -= 3000;
-      }
-    }
-    else if (index_display == 3600)
-    {
-      if((segundos[i]%36000)>36000)
-      {
-        segundos[i] -= 36000;
-      }
-    }
-    else if (index_display == 36000)
-    {
-      if(segundos[i]>72000)
-      {
-        segundos[i] -= 108000;
-      }
-    }
-//    Serial.println(segundos[REL]);*/
-  }
+//  Serial.println(segundos[TIM]);
+  //Serial.print("  ");
+  //Serial.println(temp_antes);
+// Serial.println(segundos[REL]);
   write_display();
   controle_led();
 /*  Serial.print(mode);
   Serial.print("  ");
-  Serial.print(rel_pisc);
+  Serial.print(segundos[mode]);
   Serial.print("  ");
   Serial.println(index_display);*/
 }
